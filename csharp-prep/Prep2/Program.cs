cabed68307c6e38bbc10e77e30ff6238eff8c3db
Bryce Daniels <bryceadaniels01@gmail.com>
2023-01-06 16:19:50 -0700

    Changed code to assign grade based on percentage

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,51 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+    Console.Write("What is your grade percentage? ");
+    string grade = Console.ReadLine();
+    int gradeNumber = int.Parse(grade);
+    string gradeLetter = "";
+    if (gradeNumber >= 90)
+     {
+        gradeLetter = "A";
+     }
+    else if (gradeNumber < 90 && gradeNumber >=80)
+     {
+        gradeLetter = "B";
+     }
+    else if (gradeNumber < 80 && gradeNumber >=70)
+    {
+        gradeLetter = "C";
+    }
+    else if (gradeNumber < 70 && gradeNumber >=60)
+    {
+       gradeLetter = "D";
+    }
+    else if (gradeNumber <60)
+    {
+        gradeLetter = "F";
+    }
+    string sign = "";
+    int endingDigit = gradeNumber % 10;
+    if (endingDigit >= 7 && gradeLetter != "A" && gradeLetter != "F")
+    {
+        sign = "+";
+    }
+    else if (endingDigit <3 && gradeLetter != "F")
+    {
+        sign = "-";
+    }
+
+
+    Console.WriteLine($"Your grade is {gradeLetter}{sign}");
+
+    if (gradeNumber >= 70)
+    {
+        Console.WriteLine("Congrats! Your passed!");
+    }
+    else
+    {
+        Console.WriteLine("Try again!");
+    }
     }
 }

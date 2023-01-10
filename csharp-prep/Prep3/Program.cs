13f8c03e0a5ef0b8909cb2a619bf1831c30ab4f8
Bryce Daniels <bryceadaniels01@gmail.com>
2023-01-10 08:37:08 -0700

    Created a code to do a guess a number game.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
-    }
+        string play = "yes";
+        while (play.ToLower() == "yes")
+        {
+            Console.Write("What is the magic number? ");
+            string magicNumberString = Console.ReadLine();
+            int magicNumber = int.Parse(magicNumberString);
+            int guessNumber = 0;
+            if (magicNumber >= 0)
+            {
+                guessNumber = -1;
+            }
+            else
+            {
+                guessNumber = 1;
+            }
+            int numOfGuesses = 0;
+            while (guessNumber != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                string guessString = Console.ReadLine();
+                guessNumber = int.Parse(guessString);
+                if (guessNumber > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (guessNumber < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guessNumber == magicNumber)
+                {
+                    Console.WriteLine("Correct!");
+                }
+                numOfGuesses += 1;
+            }
+            
+            Console.WriteLine($"Good job with the game. It took you {numOfGuesses} tries");
+            Console.Write("Would you like to play again? ");
+            play = Console.ReadLine();
+        }
+        Console.WriteLine("Thanks for playing!");
+    }   
 }

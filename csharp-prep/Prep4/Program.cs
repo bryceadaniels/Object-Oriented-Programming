71a59a6efcc16009c0f1781b8944846eb9226413
Bryce Daniels <bryceadaniels01@gmail.com>
2023-01-10 09:42:56 -0700

    Created code to have user input numbers for a list Calculates sum, average, largest number, smallest positive number. Sorts list

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,51 @@
 using System;
-
+using System.Collections.Generic;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        int number = -1;
+        while (number != 0)
+        {
+        Console.Write("Enter a number: ");
+        string numberString = Console.ReadLine();
+        number = int.Parse(numberString);
+        if (number != 0)
+        {
+            numbers.Add(number);
+        }
+        }
+        int sum = 0;
+        int count = numbers.Count;
+        int largestNumber = 0;
+        int smallestPositive = 100000000;
+        foreach(int numberInList in numbers)
+        {
+            sum += numberInList;
+            if (numberInList > largestNumber)
+            {
+                largestNumber = numberInList;
+            }
+            if (numberInList > 0)
+            {
+                if (numberInList < smallestPositive)
+                {
+                    smallestPositive = numberInList;
+                }
+            }
+        }
+        numbers.Sort();
+        float average = sum / count;
+        Console.WriteLine($"The sum is: {sum} ");
+        Console.WriteLine($"The average is: {average} ");
+        Console.WriteLine($"The largest number is: {largestNumber} ");
+        Console.WriteLine($"The smallest positive number is; {smallestPositive}");
+        Console.WriteLine($"The sorted list is: ");
+        foreach (int num in numbers)
+        {
+            Console.Write($"{num},");
+        }
     }
 }

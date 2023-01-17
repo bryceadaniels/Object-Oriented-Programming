667b66c7704a33385b3b0656fe66e1b10c3fe0ed
Bryce Daniels <bryceadaniels01@gmail.com>
2023-01-16 20:08:53 -0700

    Made the jobs class

---
+++ prepare/Learning02/Resume.cs
@@ -0,0 +1,12 @@
+public class Resume{
+    public string _personName = "";
+    public List<Job> _jobs = new List<Job>();
+
+    public void Display(){
+        System.Console.WriteLine($"Name: {_personName}");
+        System.Console.WriteLine("Jobs: ");
+        foreach (Job job in _jobs){
+            job.Display();
+        }
+    }
+}

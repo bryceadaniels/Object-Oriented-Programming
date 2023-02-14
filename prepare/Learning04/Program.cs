4394e9dd787be4e7571591d3d9d48cee82f3523d
Bryce Daniels <bryceadaniels01@gmail.com>
2023-02-14 09:34:23 -0700

    Done

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment1 = new Assignment("Bryce Daniels","Coding with Classes");
+        System.Console.WriteLine(assignment1.GetSummary());
+        
+        MathAssignment assignmentMath = new MathAssignment("TK","Statistics", "12", "1-29");
+        System.Console.WriteLine(assignmentMath.GetSummary());
+        System.Console.WriteLine(assignmentMath.GetHomeworkList());
+
+        WritingAssignment assignmentWriting = new WritingAssignment("Kallie", "Child Dev", "Be Good Parents");
+        System.Console.WriteLine(assignmentWriting.GetSummary());
+        System.Console.WriteLine(assignmentWriting.GetWritingInfo());
     }
 }
